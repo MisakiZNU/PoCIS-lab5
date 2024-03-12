12bb8fc106a11ca37380d3ab93d0fe9e4f2da6df
MisakiZNU <148874561+MisakiZNU@users.noreply.github.com>
2024-03-12 19:00:11 +0200

    Delete src/main/java/org/example/lab5/Repo directory

--- src/main/java/org/example/lab5/Repo/TriangleRepo.java
+++
@@ -1,8 +0,0 @@
-package org.example.lab5.repo;
-
-import org.example.lab5.models.Triangle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TriangleRepo extends JpaRepository<Triangle, Long> {
-
-}
